9a2271357b62f60b9a8d2b4bfbdaff8f6c0f6e55
西村太一 <nishimurataichi@nishimurataichi-no-iMac.local>
2017-04-09 21:57:17 +0900

    abc58 C問題までAC

--- abc58/c.cpp
+++ abc58/c.cpp
@@ -46,7 +46,61 @@ int dy[]={0,0,-1,1};
 
 int main(){
 
+    int n;
+    cin >> n;
+    string s[60];
+    vector<char> output;
+    int min = 1000;
+    int min_index;
+
+    for(int i=0; i<n; i++){
+        cin >> s[i];
+        int st_size = s[i].size();
+        if(min > st_size){
+            min = st_size;
+            min_index = i;
+        }
+    }
+
+    int roop = n-1;
+
+    for(int k=0; k<s[min_index].size(); k++){
+        bool flag = true;
+        while(roop >= 0){
+            if(roop == min_index) {
+                roop--;
+                continue;
+            }
+
+            int bas = s[roop].find(s[min_index][k]);
+            //他の文字列のいずれのときでも見つからないとき(例えば入力で言えばb,d)
+            if(bas == -1) {flag = false;}
+            else{
+                //見つかったとき
+                s[roop].replace(bas, 1, "-");
+            }
+
+            //次の文字列へ
+            roop--;
+        }
+
+        if(flag) {
+            output.push_back(s[min_index][k]);
+
+        }
+
+        //再度設定
+        roop = n - 1;
+    }
 
 
+    if(output.size() == 0){
+    }else{
+        vector<char>::iterator ite;
+        sort(output.begin(), output.end());
+        for(ite = output.begin(); ite != output.end(); ite++) cout << *ite;
+    }
+
+    cout << endl;
 
 }
